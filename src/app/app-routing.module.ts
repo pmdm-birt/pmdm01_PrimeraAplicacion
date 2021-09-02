2b28e3c97a87b38e79b6b1d29ada5485cc0e3859
Nora <nsansaturnino@birt.eus>
2021-09-02 16:39:48 +0200

    Nuevas páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+// Funciones que se usan para el loadChildren de la ruta "home"
+function callback(m) { 
+  return m.HomePageModule
+}
+
+function unaFuncion() {
+  return import('./pages/home/home.module').then( callback )
+}
+
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+
+    // Diferentes maneras de escribir la línea anterior. Con funciones lambda, anónimas o normales
+    // loadChildren: function() { return import('./home/home.module').then( m => m.HomePageModule) }
+    // loadChildren: function() { return import('./home/home.module').then( function(m) { return m.HomePageModule } ) }
+    // loadChildren: function() { return import('./home/home.module').then( callback ) }    
+    // loadChildren:  unaFuncion
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'pagina2',
     pathMatch: 'full'
+  },
+  {
+    path: 'pagina1',
+    loadChildren: () => import('./pages/pagina1/pagina1.module').then( m => m.Pagina1PageModule)
+  },
+  {
+    path: 'pagina2',
+    loadChildren: () => import('./pages/pagina2/pagina2.module').then( m => m.Pagina2PageModule)
   },
 ];
 
